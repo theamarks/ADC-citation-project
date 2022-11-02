64a5c74c736378fbdddbbdec34cd344ae0571f2f
Althea Marks <marks@nceas.ucsb.edu>
2022-11-02 15:44:00 -0700

    Groundhog not working with github package development branch. Conflicts with R versions etc. come back to

--- R/load_pkgs.R
+++ R/load_pkgs.R
@@ -1,17 +1,17 @@
 # Load packages for API queries and data analysis
-# use `groundhog` package version management 
-install.packages("groundhog")
-library("groundhog")
-meta.groundhog('2022-10-31') # Not working
-groundhog.day <-'2022-03-10'
 
-groundhog.library(
-"library(readr)
+# use `groundhog` package version management 
+#require("groundhog")
+#meta.groundhog('2022-10-31') # maintain consistent groundhog version
+#groundhog.day <-'2022-07-14'
+# Not working for now
+
+library(readr)
 library(jsonlite)
 library(tidyr)
 library(dplyr)
-library(dataone)
 library(job)
+library(dataone)
 library(magrittr)
 library(readr)
 library(stringr)
@@ -23,13 +23,13 @@ library(dataone)
 library(scales)
 library(viridis)
 library(curl)
-library(miniUI)", 
-groundhog.day,
-tolerate.R.version='4.1.3')
+library(miniUI)
+
 
 # install development version of scythe package with added xdd library
-groundhog.library('github::dataoneorg/scythe/develop' , groundhog.day, tolerate.R.version='4.1.3')
-#devtools::install_github("dataoneorg/scythe@develop")
+#groundhog.library('github::dataoneorg/scythe@develop' , groundhog.day, tolerate.R.version='4.1.3')
+devtools::install_github("dataoneorg/scythe@develop")
 
 # install dev version of rcrossref package with update API for `scythe::write_citation_pairs` dependency
-groundhog.library('github::ropensci/rcrossref', '2022-08-11', tolerate.R.version='4.1.3')
+#groundhog.library('github::ropensci/rcrossref', '2022-08-11', tolerate.R.version='4.1.3')
+devtools::install_github("ropensci/rcrossref")
