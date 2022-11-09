951f396784c00fddf89a54500b346cc8c4173647
Althea Marks <marks@nceas.ucsb.edu>
2022-11-08 17:43:15 -0800

    load scythe package from local package includes scopus search bug fix.

--- R/load_pkgs.R
+++ R/load_pkgs.R
@@ -28,7 +28,8 @@ library(miniUI)
 
 # install development version of scythe package with added xdd library
 #groundhog.library('github::dataoneorg/scythe@develop' , groundhog.day, tolerate.R.version='4.1.3')
-devtools::install_github("dataoneorg/scythe@develop")
+#devtools::install_github("dataoneorg/scythe@develop")
+devtools::load_all('~/scythe/R')
 
 # install dev version of rcrossref package with update API for `scythe::write_citation_pairs` dependency
 #groundhog.library('github::ropensci/rcrossref', '2022-08-11', tolerate.R.version='4.1.3')
