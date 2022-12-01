71bfa88f88e0475c9464887b528d35da69cfbfe1
Althea Marks <marks@nceas.ucsb.edu>
2022-12-01 11:52:37 -0800

    rearrange hierarchy script, deleted unused circle packing objects and code

--- R/ontology_hierarchy.R
+++ R/ontology_hierarchy.R
@@ -1,4 +1,8 @@
-## Build hierarchical dataframe
+# Academic Discipline Dataset Classification Analysis
+
+
+
+## Organize discipline in heirarchtical structure (network)
 
 # read in child parent relationship from ontology
 adcad <- read_csv("./data/ADCAD.csv")
@@ -20,23 +24,6 @@ disc_id <- adcad_num %>%
   select(-parents) %>% 
   rename("parents" = class_id)
 
-# create dataframe of node/vertices information
-my_vertices <- disc_id[,"label"]
-names(my_vertices)[1] <- "name"
-
-my_vertices$size <- cit_disc$n_cit[match(my_vertices$name, cit_disc$value)]
-my_vertices <- na.omit(my_vertices)
-
-all_vert <- union(unique(edges_name$from), unique(edges_name$to))
-all_vert <- data.frame(name = all_vert)
-new_vert <- all_vert %>% 
-  left_join(my_vertices)
-new_vert[is.na(new_vert$size), "size"] <- 0
-
-# Leafs must have a weight
-vert_hydro_leaves <- new_vert %>% 
-  filter(size > 0)
-
 
 # create dataframe with edge/connector info
 edges_num <- adcad_num[,c(1,3)] %>% 
@@ -55,14 +42,24 @@ edges_name %<>%
   na.omit() %>% 
   mutate(from = ifelse(from == "Academic Discipline", "origin", from)) # %>% 
 
-# add new leaves for hydrology - remove old structure
+# add new leaves for hydrology - remove old structure - edge df
 new_leaves <- data.frame("from" = "Geoscience","to" = c("Cryology", "Glaciology"))
 dead_leaves <- edges_name[-which(edges_name$to == c("Cryology", "Glaciology")),]
 hydro_leaves <- rbind(dead_leaves, new_leaves)
 
-edges_hydro_leaves <- hydro_leaves %>% 
-  filter(to %in% c(vert_hydro_leaves$name))
 
-add_mid_nodes <- data.frame(name = unique(edges_hydro_leaves$from), size = 0)
-vert_hydro_leaves <- rbind(vert_hydro_leaves, add_mid_nodes)
+# create dataframe of node/vertices information
+my_vertices <- disc_id[,"label"]
+names(my_vertices)[1] <- "name"
+
+
+# create vertices df - describes values for leaves (end points)
+my_vertices$size <- cit_disc$n_cit[match(my_vertices$name, cit_disc$value)]
+my_vertices <- na.omit(my_vertices)
+
+all_vert <- union(unique(edges_name$from), unique(edges_name$to))
+all_vert <- data.frame(name = all_vert)
+new_vert <- all_vert %>% 
+  left_join(my_vertices)
+new_vert[is.na(new_vert$size), "size"] <- 0
 
