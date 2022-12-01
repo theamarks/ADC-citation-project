5d48c0a027e3773a3c297e4a48ba7ab964d47652
Althea Marks <marks@nceas.ucsb.edu>
2022-11-30 17:51:10 -0800

    Working second circle packing figure. reorganized hierarchy, not completely accurate

--- R/ontology_hierarchy.R
+++ R/ontology_hierarchy.R
@@ -38,17 +38,6 @@ vert_hydro_leaves <- new_vert %>%
   filter(size > 0)
 
 
-
-# this works in circle packing with edges_hydro
-# hydro <- new_vert %>% 
-#   filter(name %in% c("Hydrology", "Cryology", "Glaciology")) 
-# hydro_combine <- data.frame("name" = "Hydrology", "size" = sum(hydro$size))
-
-# hydro_vert <- new_vert %>% 
-#   filter(!(name %in% c("Hydrology", "Cryology", "Glaciology")),
-#          size > 0) 
-# hydro_vert <- rbind(hydro_vert, hydro_combine)
-
 # create dataframe with edge/connector info
 edges_num <- adcad_num[,c(1,3)] %>% 
   rename("from" = "parents",
@@ -66,11 +55,6 @@ edges_name %<>%
   na.omit() %>% 
   mutate(from = ifelse(from == "Academic Discipline", "origin", from)) # %>% 
 
-# combines hydrology categories all under hydrology - works for circle packing
-# edges_hydro <- edges_name %>% 
-#   filter(!(to %in% c("Cryology", "Glaciology")),
-#          to %in% c(hydro_vert$name))
-
 # add new leaves for hydrology - remove old structure
 new_leaves <- data.frame("from" = "Geoscience","to" = c("Cryology", "Glaciology"))
 dead_leaves <- edges_name[-which(edges_name$to == c("Cryology", "Glaciology")),]
@@ -82,6 +66,3 @@ edges_hydro_leaves %<>%
 add_mid_nodes <- data.frame(name = unique(edges_hydro_leaves$from), size = 0)
 vert_hydro_leaves <- rbind(vert_hydro_leaves, add_mid_nodes)
 
-# part of working circle packing - combine hydrology 
-# high_nodes <- data.frame("name" = unique(edges_hydro$from), "size" = 0)
-# hydro_vert <- rbind(hydro_vert, high_nodes)
